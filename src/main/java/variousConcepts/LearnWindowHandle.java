e21a61e7f1e944f296cca74f40832d1ed90ba34f
Gelila12 <75943633+Gelila12@users.noreply.github.com>
2021-10-04 17:54:00 -0500

    first commit

--- src/main/java/variousConcepts/LearnWindowHandle.java
+++ src/main/java/variousConcepts/LearnWindowHandle.java
@@ -17,28 +17,34 @@ public class LearnWindowHandle {
 	System.setProperty("webdriver.chrome.driver",".\\driver\\chromedriver.exe");
     driver = new ChromeDriver();
 	driver.get("https://www.yahoo.com/");
-	driver.manage().window().maximize();
+	//driver.manage().window().maximize();
 	driver.manage().deleteAllCookies();
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	
-	
-	//System.out.println(driver.getTitle());
-	//System.out.println(driver.getWindowHandle());
-	
-	driver.findElement(By.xpath("//*[@id=\"ybar-sbq\"]")).sendKeys("xpath");
-	driver.findElement(By.xpath("//*[@id=\"ybar-search\"]")).click();
 	
 	System.out.println(driver.getTitle());
 	String handle = driver.getWindowHandle();
 	System.out.println(handle);
 	
+	
+	driver.findElement(By.xpath("//*[@id=\"ybar-sbq\"]")).sendKeys("xpath");
+	driver.findElement(By.xpath("//*[@id=\"ybar-search\"]")).click();
+	
+	
 	driver.findElement(By.linkText("XPath Tutorial - W3Schools")).click();
+	
 	
 	System.out.println(driver.getTitle());
 	Set<String> handles = driver.getWindowHandles();
 	System.out.println(handles);
-	
-	
+	 
+	for(String i : handles ) {
+		System.out.println(i);
+		driver.switchTo().window(i);
+	}
+
+	System.out.println(driver.getTitle());
+	driver.switchTo().window(handle);
+	System.out.println(driver.getTitle());
 }
 
 }
